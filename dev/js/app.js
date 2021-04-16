e0af962d6aaf764e0c9fe9ce458228138a80b44f
iancatania <78445705+iancatania@users.noreply.github.com>
2021-04-16 15:19:51 -0400

    Final styling changes in JS

--- dev/js/app.js
+++ dev/js/app.js
@@ -7,11 +7,21 @@ let sproutRiceAmount = CookingAmount;
 let sproutRiceWater = sproutRiceAmount * 1.6;
 
 function WhiteRice() {
+  document.getElementById("WhiteRice").style.width = "550px";
+  document.getElementById("WhiteRice").style.marginLeft = "500px";
+  document.getElementById("WhiteRice").style.textAlign = "center";
+  document.getElementById("WhiteRice").style.background = "white";
+  document.getElementById("WhiteRice").style.border = "solid black";
   document.getElementById("WhiteRice").innerHTML = "Combine " + String(whiteRiceAmount) + " cup(s) of rice with " + String(whiteRiceWater) +  " cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
   document.getElementById("SproutRice").innerHTML = "";
 }
 
 function SproutRice() {
+  document.getElementById("SproutRice").style.width = "550px";
+  document.getElementById("SproutRice").style.marginLeft = "500px";
+  document.getElementById("SproutRice").style.textAlign = "center";
+  document.getElementById("SproutRice").style.background = "white";
+  document.getElementById("SproutRice").style.border = "solid black";
   document.getElementById("SproutRice").innerHTML = "For slightly al dente rice: Combine " +  String(sproutRiceAmount) + " cups of rice with " +  String(sproutRiceWater) +  " cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by " + String(sproutRiceAmount) + " cup and cook time by 5 minutes.";
   document.getElementById("WhiteRice").innerHTML = "";
 }
