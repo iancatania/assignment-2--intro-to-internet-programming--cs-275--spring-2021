ef4b3b6c39ca65be3651bd62d5811d3157de9819
iancatania <78445705+iancatania@users.noreply.github.com>
2021-04-15 21:44:26 -0400

    Finished button support

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,16 +1,16 @@
 let CookingAmount = prompt("How much rice do you want to cook?", "1.25");
 
 let number = parseFloat(CookingAmount);
-let whiteRiceTotal = number  * 1.6;
-let sproutRiceTotal = number * 2;
+let whiteRiceAmount = number  * 1.6;
+let sproutRiceAmount = number * 2;
 
 function WhiteRice() {
-  document.getElementById("WhiteRice").innerHTML = "Combine " + String(whiteRiceTotal) + " cup(s) of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
-  document.getElementById("WhiteRice").innerHTML = "";
+  document.getElementById("WhiteRice").innerHTML = "Combine " + String(whiteRiceAmount) + " cup(s) of rice with 2 cups of water and 1 Tbsp olive oil. Bring to a boil, then reduce heat to the lowest setting. Cook for about 18 minutes.";
+  document.getElementById("SproutRice").innerHTML = "";
 }
 
 function SproutRice() {
-  document.getElementById("SproutRice").innerHTML = "For slightly al dente rice: Combine " +  String(sproutRiceTotal) + " cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by " + String(sproutRiceTotal) + " cup and cook time by 5 minutes.";
+  document.getElementById("SproutRice").innerHTML = "For slightly al dente rice: Combine " +  String(sproutRiceAmount) + " cups of rice with 2 cups of water or broth and 1 Tbsp olive oil. Bring to a boil and stir once to mix. Reduce heat to low, cover with a tight-fitting lid and cook for 25 minutes. Remove from heat and let stand for 5 minutes. Fluff with a fork and serve. For softer rice: Increase liquid by " + String(sproutRiceAmount) + " cup and cook time by 5 minutes.";
   document.getElementById("WhiteRice").innerHTML = "";
 }
 
